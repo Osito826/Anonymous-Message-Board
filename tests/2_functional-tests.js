7cf9b2015a95e78c8813a14363ccd64e9525353c
Glitch (sepia-mire-azimuth) <none>
2023-09-13 23:28:37 +0000

    🍇💆 Checkpoint ./tests/2_functional-tests.js:59675918/542

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,26 +6,5 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', function() {
-  let testThreadId;
-  let testReplyId;
-  let testPass = "testpass";
-
-  test("Create a New Thread", (done) => {
-    chai
-      .request(server)
-      .post("/api/threads/test")
-      .send({
-        board: "test",
-        text: "Functional Test Thread",
-        delete_password: testPass,
-      })
-      .end((err, res) => {
-        assert.equal(res.status, 200);
-        let createdThreadId =
-          res.redirects[0].split("/")[res.redirects[0].split("/").length - 1];
-        testThreadId = createdThreadId;
-        done();
-      });
-  });
-
+  
 });
