33debd827d8b608d1e4c24e73d988ea16a814d9b
Glitch (sepia-mire-azimuth) <none>
2023-09-15 05:05:57 +0000

    🎯🙎 Checkpoint ./routes/api.js:59675918/13158

--- routes/api.js
+++ routes/api.js
@@ -42,22 +42,24 @@ module.exports = function (app) {
   const Thread = mongoose.model("Thread", threadSchema);
   const Board = mongoose.model("Board", boardSchema);
 
-  app.route("/api/threads/:board").post(async (request, response) => {
-    const { text, delete_password } = request.body;
-    const { board } = request.params
-    //let board = request.body.board;
-    
-    //let currentDate = new Date();
-    const newThread = await Thread.create({
-      board,
-      text,
-      delete_password,
-      //created_on: currentDate,
-      //bumped_on: currentDate,
-      replies: [],
-    });
-    console.log(newThread);
-    
+  app
+    .route("/api/threads/:board")
+    .post(async (request, response) => {
+      const { text, delete_password } = request.body;
+      const { board } = request.params;
+      //let board = request.body.board;
+
+      //let currentDate = new Date();
+      const newThread = await Thread.create({
+        board,
+        text,
+        delete_password,
+        //created_on: currentDate,
+        //bumped_on: currentDate,
+        replies: [],
+      });
+      console.log(newThread);
+
       const boardData = await Board.findOne({ name: board });
       if (!boardData) {
         const newBoard = new Board({
@@ -68,45 +70,45 @@ module.exports = function (app) {
         console.log(newBoard);
         const data = await newBoard.save();
         if (data) {
-          response.send("There was an error saving in post");
-        } else {
-          response.json(newThread);
+          response.send(newThread);
         }
       } else {
         boardData.threads.push(newThread);
         const data = await boardData.save();
-        if (!data) {
-          response.send("There was an error saving in post");
-        } else {
-          response.json(newThread);
+        if (data) {
+          response.send(newThread);
         }
       }
-    } catch (err) {
-      console.log(err);
-    }
-  })
-  .get(async (req, res) => {
-      // GET ROUTE
-      const { board } = req.params
-      let threads = await Thread.find({ board }).sort("-bumped_on").populate("replies")
-
-      threads = threads.map(thread => {
-        let threadToView = {
-          _id: thread._id,
-          text: thread.text,
-          created_on: thread.created_on,
-          bumped_on: thread.bumped_on,
-          replies: thread.replies.sort((a, b) => a.created_on - b.created_on).slice(0, 3).map(reply => {
-            let rep = {
-              _id: reply._id,
-              text: reply.text,
-              created_on: reply.created_on,
-            }
-            return rep
-          }),
-        }
-        return threadToView
-      }).slice(0, 10)
-      res.send(threads)
     })
+    .get(async (req, res) => {
+      // GET ROUTE
+      const { board } = req.params;
+      let threads = await Thread.find({ board })
+        .sort("-bumped_on")
+        .populate("replies");
+
+      threads = threads
+        .map((thread) => {
+          let threadToView = {
+            _id: thread._id,
+            text: thread.text,
+            created_on: thread.created_on,
+            bumped_on: thread.bumped_on,
+            replies: thread.replies
+              .sort((a, b) => a.created_on - b.created_on)
+              .slice(0, 3)
+              .map((reply) => {
+                let rep = {
+                  _id: reply._id,
+                  text: reply.text,
+                  created_on: reply.created_on,
+                };
+                return rep;
+              }),
+          };
+          return threadToView;
+        })
+        .slice(0, 10);
+      res.send(threads);
+    });
 };
