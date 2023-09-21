a88163d917b7d5d579d12b21a75a9f2d182f45f2
Glitch (sepia-mire-azimuth) <none>
2023-09-21 20:15:09 +0000

    🗺🎇 Checkpoint ./tests/2_functional-tests.js:59675918/393

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -54,4 +54,17 @@ suite("Functional Tests", function () {
         done();
       });
   });
+  
+  test("Delete: Deleting a thread with the correct password", function (done){
+    chai
+    .request(server)
+    .delete("/api/threads/test")
+    .send(threadPostData)
+    .end((err, res) => {
+      assert.equal(res.status, 200)
+      assert.equal(res.body.delete_password, "success")
+      done();
+    })
+    
+  })
 });
