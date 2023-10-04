3132a62308698a408f17059ab626812752b612ea
Glitch (sepia-mire-azimuth) <none>
2023-10-04 04:44:27 +0000

    🚤🐑 Checkpoint ./tests/2_functional-tests.js:59675918/2468

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -24,61 +24,7 @@ suite("Functional Tests", function () {
         done();
       });
   });
-
-  test("GET: Viewing the 10 most recent threads with 3 replies each", function (done) {
-    chai
-      .request(server)
-      .get("/api/threads/test")
-      .end(function (err, res) {
-        assert.equal(res.status, 200);
-        assert.isArray(res.body);
-        assert.isObject(res.body[0]);
-        assert.isDefined(res.body[0].text);
-        assert.isDefined(res.body[0].created_on);
-        assert.isDefined(res.body[0].bumped_on);
-        assert.isArray(res.body[0].replies);
-        assert.isBelow(res.body[0].replies.length, 4);
-        done();
-      });
-  });
-
-  test("Delete: Deleting a thread with the incorrect password", function (done) {
-    chai
-      .request(server)
-      .delete("/api/threads/test")
-      //.set("content-type", "application/json")
-      .send({ thread_id: testThread_id, delete_password: "incorrect" })
-      .end((err, res) => {
-        assert.equal(res.status, 200);
-        assert.equal(res.text, "incorrect password");
-        done();
-      });
-  });
   
-  test("Put: Reporting a thread", function (done) {
-    chai
-      .request(server)
-      .put("/api/threads/test")
-      .send({ ...threadPostData, thread_id: testThread_id })
-      .end((err, res) => {
-        assert.equal(res.status, 200);
-        assert.equal(res.text, "reported");
-        done();
-      });
-  });
-
-  test("Delete: Deleting a thread with the correct password", function (done) {
-    chai
-      .request(server)
-      .delete("/api/threads/test")
-      .send({ ...threadPostData, thread_id: testThread_id })
-      .end((err, res) => {
-        assert.equal(res.status, 200);
-        assert.equal(res.text, "success");
-        done();
-      });
-  });
-
   test("Post: Creating a new reply", function (done) {
     chai
       .request(server)
@@ -96,6 +42,23 @@ suite("Functional Tests", function () {
       });
   });
 
+  test("GET: Viewing the 10 most recent threads with 3 replies each", function (done) {
+    chai
+      .request(server)
+      .get("/api/threads/test")
+      .end(function (err, res) {
+        assert.equal(res.status, 200);
+        assert.isArray(res.body);
+        assert.isObject(res.body[0]);
+        assert.isDefined(res.body[0].text);
+        assert.isDefined(res.body[0].created_on);
+        assert.isDefined(res.body[0].bumped_on);
+        assert.isArray(res.body[0].replies);
+        assert.isBelow(res.body[0].replies.length, 4);
+        done();
+      });
+  });
+  
   test("Get: Viewing a single thread with all replies", function (done) {
     chai
       .request(server)
@@ -104,6 +67,43 @@ suite("Functional Tests", function () {
       .end((err, res) => {
         assert.equal(res.status, 200);
         assert.isArray(res.body.replies);
+        done();
+      });
+  });
+  
+  test("Put: Reporting a thread", function (done) {
+    chai
+      .request(server)
+      .put("/api/threads/test")
+      .send({ ...threadPostData, thread_id: testThread_id })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, "reported");
+        done();
+      });
+  });
+
+  test("Delete: Deleting a thread with the incorrect password", function (done) {
+    chai
+      .request(server)
+      .delete("/api/threads/test")
+      //.set("content-type", "application/json")
+      .send({ thread_id: testThread_id, delete_password: "incorrect" })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, "incorrect password");
+        done();
+      });
+  });
+
+  test("Delete: Deleting a thread with the correct password", function (done) {
+    chai
+      .request(server)
+      .delete("/api/threads/test")
+      .send({ ...threadPostData, thread_id: testThread_id })
+      .end((err, res) => {
+        assert.equal(res.status, 200);
+        assert.equal(res.text, "success");
         done();
       });
   });
